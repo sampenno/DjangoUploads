d2d36acc285d507343425fbc2b856d5e75cb1b0d
sampenno <48610839+sampenno@users.noreply.github.com>
2024-12-19 12:59:52 +1100

    Delete django-uploads/djangouploads directory

--- django-uploads/djangouploads/djangouploads/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from djangouploads.models import Movie
-
-class MovieAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-
-admin.site.register(Movie, MovieAdmin)
-
